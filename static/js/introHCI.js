3d0f4c4202116c122085e39663d724676308790e
Amanda Quach <amquach00@gmail.com>
2021-01-21 10:47:56 -0800

    final

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,17 +2,29 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+    initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+    $("#testjs").click(function(e) {
+        $('.jumbotron h1').text("Ask your own heart.");
+    });
+    $(".jumbotron p").toggleClass("active");
+    $("#testjs").text("Please wait...");
+    $("a.thumbnail").click(projectClick);
+}
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+function projectClick(e) {
+    // Add any additional listeners here
+    // example: $("#div-id").click(functionToCall);
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
 }
